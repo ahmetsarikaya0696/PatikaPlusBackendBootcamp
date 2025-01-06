a50deca70b04e382c655b7762692f65648ee2f4d
ahmet sarikaya <ahmetsarikaya0696@gmail.com>
2025-01-06 23:25:10 +0300

    Pratik20

---
+++ Pratik20/Pratik20/Program.cs
@@ -0,0 +1,27 @@
+﻿List<string> coffees = new List<string>();
+string input = string.Empty;
+int index = 1;
+
+while (true)
+{
+    Console.Write($"Kahve {index} : ");
+    input = Console.ReadLine();
+
+    coffees.Add(input);
+
+    Console.Write("Başka kahve eklemek istiyorsanız herhangi bir tuşa istemiyorsanız 'h' tuşuna basınız : ");
+    input = Console.ReadLine().ToLower();
+
+    if (input == "h")
+    {
+        Console.WriteLine("Programdan çıkılıyor...");
+        break;
+    }
+    index++;
+}
+
+Console.WriteLine("Girilen Kahve İsimleri:");
+foreach (var coffee in coffees)
+{
+    Console.WriteLine(coffee);
+}
---
+++ Pratik20/Pratik20/README.md
@@ -0,0 +1,9 @@
+# Pratik20
+
+## Description
+This program allows users to input names of different types of coffee. The user can continue to add coffee names until they decide to stop by pressing 'h'. Once the user decides to stop, the program will display all the coffee names that were entered.
+
+## Features
+- Continuously prompts the user to enter coffee names.
+- Allows the user to stop entering names by pressing 'h'.
+- Displays all entered coffee names at the end.
