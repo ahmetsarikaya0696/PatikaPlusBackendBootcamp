fcf2d63bfffa1d729a3d12780b7fb74f98fc4c5b
ahmet sarikaya <ahmetsarikaya0696@gmail.com>
2025-01-06 23:11:10 +0300

    Pratik19

---
+++ Pratik19/Pratik19/Program.cs
@@ -0,0 +1,26 @@
+﻿List<string> guests = new List<string>(); // Create a list to store guests
+var input = string.Empty; // Variable to store user input
+
+while (true)
+{
+    // Retrieve guest name
+    Console.Write("Lütfen davet etmek istediğiniz kişinin adını ve soyadını giriniz: ");
+    input = Console.ReadLine();
+
+    // Add guest to the list
+    guests.Add(input);
+
+    // Ask user if they want to add another guest
+    Console.Write("Başka bir davetli eklemek istiyor musunuz? (Evet için herhangi bir tuşa/Hayır için h tuşuna basınız): ");
+    input = Console.ReadLine();
+
+    // If user enters "h", exit the loop
+    if (input.ToLower() == "h") break;
+}
+
+// Display guests
+Console.WriteLine("** Davetliler **");
+foreach (var guest in guests)
+{
+    Console.WriteLine(guest);
+}
---
+++ Pratik19/Pratik19/README.md
@@ -0,0 +1,11 @@
+# Pratik19
+
+## Overview
+
+Pratik19 is a .NET 8 project designed to manage a guest list for an event.
+
+## Features
+
+- Add guests to the list by entering their names.
+- Option to add multiple guests.
+- Display the list of all guests added.
