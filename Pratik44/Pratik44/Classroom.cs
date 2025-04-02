6e4417957f182af21207ce22cb88cf9e5afc4bb3
ahmet sarikaya <ahmetsarikaya0696@gmail.com>
2025-04-02 15:01:21 +0300

    Pratik 44

---
+++ Pratik44/Pratik44/Classroom.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pratik44
+{
+    public class Classroom
+    {
+        public ITeacher Teacher { get; set; }
+
+        public Classroom(ITeacher teacher)
+        {
+            Teacher = teacher;
+        }
+
+        public string GetTeacherInfo()
+        {
+            return Teacher.GetInfo();
+        }
+    }
+}
---
+++ Pratik44/Pratik44/Program.cs
@@ -0,0 +1,7 @@
+﻿using Pratik44;
+
+ITeacher teacher = new Teacher("Sertan", "Bozkuş");
+
+Classroom classroom = new Classroom(teacher);
+
+Console.WriteLine("Sınıfın öğretmeni: " + classroom.GetTeacherInfo());
